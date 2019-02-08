293e3411764e5fabc17097598cb40358fd97c242
Loki Verloren <l0k18@protonmail.com>
2019-02-08 04:53:34 +0100

    almost finished parsers

---
+++ defs.go
@@ -0,0 +1,116 @@
+package skele
+
+import (
+	"time"
+)
+
+// Skeller is the interface defining an API interface item and metadata
+type Skeller interface {
+	Name() string
+	Authors() []string
+	Version() string
+	Licence() string
+	Init(...interface{})
+	Brief() string
+	Help(string) string
+}
+
+// Int is a skele integer type
+type Int int
+
+// Float is a skele float type
+type Float float64
+
+// Duration is a skele duration type
+type Duration time.Duration
+
+// Time is a skele time type
+type Time time.Time
+
+// Date is a skele date type
+type Date time.Time
+
+// Size is a size in bytes, can be specified with K/k M/m G/g T/t P/p
+type Size int
+
+// String is a string
+type String string
+
+// URL is a string describing a file in URL format
+type URL string
+
+// Address is a string describing a network address
+type Address string
+
+// Base58 is a Base58check string like a cryptocurrency address
+type Base58 string
+
+// Base32 is a base32 encoding as is used with some cryptocurrencies and cryptographic tools, not case sensitive but follows standard
+type Base32 string
+
+// Hex is a hexadecimal string, not case sensitive
+type Hex string
+
+var (
+	// HelpTypes are the different formats a help text can be encoded in
+	HelpTypes = []string{
+		"pre",
+		"markdown",
+		"html",
+	}
+
+	ptInt      Int
+	ptFloat    Float
+	ptDuration Duration
+	ptTime     Time
+	ptSize     Size
+	ptString   String
+	ptURL      URL
+	ptAddress  Address
+	ptBase58   Base58
+	ptBase32   Base32
+	ptHex      Hex
+
+	// ParamTypes allows recognition the types with the names using type switches
+	ParamTypes = []Pair{
+		{"int", ptInt},
+		{"float", ptFloat},
+		{"duration", ptDuration},
+		{"time", ptTime},
+		{"size", ptSize},
+		{"string", ptString},
+		{"url", ptURL},
+		{"address", ptAddress},
+		{"base58", ptBase58},
+		{"base32", ptBase32},
+		{"hex", ptHex},
+	}
+)
+
+// A Pair is a label that allows reflection-free runtime typing without type switches (strings instead)
+type Pair struct {
+	T string
+	V interface{}
+}
+
+// Type returns the type of a pair as a string
+func (p *Pair) Type() string {
+	return p.T
+}
+
+// Value returns the value of a Pair
+func (p *Pair) Value() interface{} {
+	return p.V
+}
+
+// Cmd is a command,
+type Cmd struct {
+	name    string
+	authors []string
+	version string
+	licence string
+	inits   []func(...interface{}) error
+	brief   string
+	help    map[string]string
+	Pairs   []Pair
+}
---
+++ parse.go
@@ -0,0 +1,170 @@
+package skele
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// ParseInt tries to read an integer from a string
+func ParseInt(in string) (out Int, err error) {
+	var i int64
+	i, err = strconv.ParseInt(in, 10, 64)
+	return Int(i), err
+}
+
+// ParseFloat tries to read a floating point number from a string
+func ParseFloat(in string) (out Float, err error) {
+	var i float64
+	i, err = strconv.ParseFloat(in, 64)
+	return Float(i), err
+}
+
+// ParseDuration takes a string and tries to read a duration in Golang time.Duration format
+func ParseDuration(in string) (out Duration, err error) {
+	o, err := time.ParseDuration(in)
+	return Duration(o), err
+}
+
+// ParseTime takes a string and tries to read a time specification from it (time of day)
+func ParseTime(in string) (out Time, err error) {
+	t, err := time.Parse("15:04:05", in)
+	return Time(t), err
+}
+
+// ParseDate takes a string and tries to read a date in yyyy-mm-dd format
+func ParseDate(in string) (out Date, err error) {
+	t, err := time.Parse("2006-01-02", in)
+	return Date(t), err
+}
+
+const (
+	zero int64 = 0
+	one  int64 = 1
+	kB   int64 = 1024
+	mB   int64 = 1024 * kB
+	gB   int64 = 1024 * mB
+	tB   int64 = 1024 * gB
+	pB   int64 = 1024 * tB
+
+	kiB int64 = 1000
+	miB int64 = 1000 * kiB
+	giB int64 = 1000 * miB
+	tiB int64 = 1000 * giB
+	piB int64 = 1000 * tiB
+)
+
+// ParseSize accepts a string and returns a value representing bytes, using the following annotations:
+// kKmMgGtTpP single letter for power of 2 based size
+// kb/mb/gb/tb/pb case insensitive ^2 based size
+// kib/mib/gib/tib/pib case insensitive 10 based size
+func ParseSize(in string) (out Size, err error) {
+	unit := one
+	var ii int64
+	ii, err = strconv.ParseInt(in, 10, 64)
+	if err == nil {
+		return Size(ii), nil
+	}
+	if len(in) > 1 {
+		last1 := in[len(in)-1]
+		switch last1 {
+		case 'k', 'K':
+			unit = kB
+		case 'm', 'M':
+			unit = mB
+		case 'g', 'G':
+			unit = gB
+		case 't', 'T':
+			unit = tB
+		case 'p', 'P':
+			unit = pB
+		default:
+			goto two
+		}
+		ii, err = strconv.ParseInt(in[:len(in)-1], 10, 64)
+		if err == nil {
+			out = Size(unit * ii)
+		}
+		return
+	}
+two:
+	if len(in) > 2 {
+		last2 := strings.ToLower(in[len(in)-2 : len(in)-1])
+		switch last2 {
+		case "kb":
+			unit = kB
+		case "mb":
+			unit = mB
+		case "gb":
+			unit = gB
+		case "tb":
+			unit = tB
+		case "pb":
+			unit = pB
+		default:
+			goto three
+		}
+		ii, err = strconv.ParseInt(in[:len(in)-2], 10, 64)
+		if err == nil {
+			out = Size(unit * ii)
+		}
+		return
+	}
+three:
+	if len(in) > 3 {
+		last3 := strings.ToLower(in[len(in)-3 : len(in)-1])
+		switch last3 {
+		case "kb":
+			unit = kiB
+		case "mb":
+			unit = miB
+		case "gb":
+			unit = giB
+		case "tb":
+			unit = tiB
+		case "pb":
+			unit = piB
+		default:
+			goto four
+		}
+		ii, err = strconv.ParseInt(in[:len(in)-3], 10, 64)
+		if err == nil {
+			out = Size(unit * ii)
+		}
+		return
+	four:
+		err = errors.New("did not decode a size from the string")
+	}
+	return
+}
+
+// ParseURL takes a string and tries to construct a full URL from it based on assuming http from first slash after domain name, can include a port
+func ParseURL(in string) (out URL, err error) {
+
+	return
+}
+
+// ParseAddress takes a string and tries to get an IPv4 or IPv6 address
+func ParseAddress(in string) (out Address, err error) {
+
+	return
+}
+
+// ParseBase58 takes a string and tries to read a base58check binary value
+func ParseBase58(in string) (out Base58, err error) {
+
+	return
+}
+
+// ParseBase32 takes a string and tries to read base32 from it
+func ParseBase32(in string) (out Base32, err error) {
+
+	return
+}
+
+// ParseHex takes a string and tries to read a hex string, including potentially the 0x prefix
+func ParseHex(in string) (out Hex, err error) {
+
+	return
+}
