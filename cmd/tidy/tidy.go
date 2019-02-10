f33266ec4cba0e7d4507d4fe5256445cb962fa69
Loki Verloren <l0k18@protonmail.com>
2019-02-10 11:24:18 +0100

    almost finishied tidyer

--- cmd/tidy/tidy.go
+++ cmd/tidy/tidy.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -40,11 +41,148 @@ usage:
 `, os.Args[0])
 }
 
+var sections [][]string
+
+const (
+	_p = iota
+	_i
+	_t
+	_c
+	_v
+	_f
+)
+
 func tidyFile() {
-	fmt.Println("tidying file", os.Args[1])
 	b, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print(string(b))
+	s := string(b)
+	lines := strings.Split(s, "\n")
+	for i, x := range lines {
+		if len(x) < 1 && i < len(lines) {
+			lines = append(lines[:i], lines[i+1:]...)
+		}
+	}
+	var keyLines []int
+	var keyText []string
+	for i, x := range lines {
+		switch {
+		case hasRootKeyword(x):
+			keyLines = append(keyLines, i)
+			keyText = append(keyText, x)
+		}
+	}
+	li := getLineIter(lines)
+	var adjusted []int
+	for i, x := range keyLines {
+		if i == 0 {
+			continue
+		}
+		li.i = x
+		for li.prev(); isComment(li.get()); li.prev() {
+		}
+		adjusted = append(adjusted, li.i+1)
+	}
+	var i, prev int
+	sections = make([][]string, 6)
+	for i = range adjusted {
+		l := lines[prev:adjusted[i]]
+		prev = adjusted[i]
+		var section []string
+		for _, x := range l {
+			section = append(section, x)
+		}
+		section = append(section, "")
+		switch lines[keyLines[i]][0] {
+		case 'p':
+			sections[_p] = append(sections[_p], section...)
+		case 'i':
+			sections[_i] = append(sections[_i], section...)
+		case 't':
+			sections[_t] = append(sections[_t], section...)
+		case 'c':
+			sections[_c] = append(sections[_c], section...)
+		case 'v':
+			sections[_v] = append(sections[_v], section...)
+		case 'f':
+			sections[_f] = append(sections[_f], section...)
+		}
+	}
+	// fmt.Println(sections)
+	for i := range sections {
+		for _, x := range sections[i] {
+			fmt.Println(x)
+		}
+	}
+}
+
+func isComment(l string) bool {
+	if len(l) > 1 {
+		x := 0
+		// skip spaces
+		for ; charIsOneOf(l[x], '\t', ' '); x++ {
+		}
+		if l[x:x+2] == "//" {
+			return true
+		}
+	}
+	return false
+}
+
+var keywords = []string{
+	"package", "import", "type", "const", "var", "func",
+}
+
+func hasRootKeyword(l string) bool {
+	for _, x := range keywords {
+		if len(x) <= len(l) {
+			if x == l[:len(x)] {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func charIsOneOf(a byte, b ...byte) bool {
+	for _, x := range b {
+		if x == a {
+			return true
+		}
+	}
+	return false
+}
+
+type iL struct {
+	ss []string
+	i  int
+	bool
+}
+
+func getLineIter(s []string) iL {
+	return iL{s, 0, true}
+}
+
+func (r *iL) next() string {
+	r.bool = true
+	if r.i < len(r.ss)-1 {
+		r.i++
+		return r.ss[r.i]
+	}
+	r.bool = false
+	return ""
+}
+
+func (r *iL) prev() string {
+	r.bool = true
+	if r.i > 0 {
+		r.i--
+		return r.ss[r.i]
+	}
+	return ""
+}
+
+func (r *iL) get() string {
+	return r.ss[r.i]
 }
